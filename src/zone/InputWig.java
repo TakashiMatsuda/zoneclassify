09270da784573d1277edc7975e11ec197790f81f
Takashi Matsuda <mazda.takasky@gmail.com>
2012-11-14 13:07:49 +0900

    Let's noteより、プッシュ忘れ。これを標準masterとする。

--- src/zone/InputWig.java
+++ src/zone/InputWig.java
@@ -5,15 +5,15 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 public class InputWig {
-	public static ArrayList<Byte> read(String filename){
-		ArrayList<Byte> harvest = new ArrayList<Byte>();
+	public static ArrayList<double> read(String filename){
+		ArrayList<Double> harvest = new ArrayList<Double>();
 		try{
 			// ArrayListで読み取るのが早いか、それとも
 			// 配列で読むために一度回して行数を取得したほうが早いか
 			String line = null;
 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			while((line = br.readLine()) != null){
-				harvest.add((byte)line.toCharArray()[0]);// ここ怪しい、文法最チェック
+				harvest.add(Double.parseDouble(line));
 			}
 			br.close();
 		} catch(Exception e){
