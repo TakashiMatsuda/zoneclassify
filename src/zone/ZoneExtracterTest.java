b33b268c630fe36f6ed6e7a9ad9d7f7ebef5a5b5
Takashi Matsuda <mazda.takasky@gmail.com>
2012-11-30 15:11:24 +0900

    a

---
+++ src/zone/ZoneExtracterTest.java
@@ -0,0 +1,23 @@
+package zone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZoneExtracterTest {
+
+	@Test
+	public void testSubZone() {
+		
+		ZoneExtracter exa = new ZoneExtracter();
+		exa.subZone(10);
+		
+	}
+
+	@Test
+	public void testZoneExtracter() {
+		ZoneExtracter exa = new ZoneExtracter();
+
+	}
+
+}
