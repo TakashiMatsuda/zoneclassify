1bdccfdb39a51130ee3dc3c88dbc7ca6eb02f11e
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-02 19:51:59 +0900

    LoadのTest開始。テストコードを書いている途中です

---
+++ src/zone/DataSetTest.java
@@ -0,0 +1,41 @@
+package zone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataSetTest {
+
+	@Test
+	public void testLoad() {
+		DataSet exa = new DataSet();
+		exa.load(zones, filename);// これから
+	}
+
+	@Test
+	public void testInitWeight() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testErrorRatio() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testWeakLearn() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testReviseWeight() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testDataSet() {
+		DataSet exa = new DataSet();
+		
+	}
+
+}
