3fd3a61b498712a09151f601d8bccedf198a5454
Takashi Matsuda <mazda.takasky@gmail.com>
2013-01-06 22:26:18 +0900

    Seg~に問題を書き込みした

--- src/zone/SegmentSetsExtracter.java
+++ src/zone/SegmentSetsExtracter.java
@@ -7,16 +7,22 @@ import java.util.List;
  * ZoneExtracterに代わり区間抽出を行うクラス
  * 基本的な部分は引き継ぐべき
  * 最短区間長制限問題をつくアルゴリズムを実装しています。
+ * static に用いられる
  * @author takashi
  *
  */
 public class SegmentSetsExtracter {
-
-	public SegmentSetsExtracter() {
-		// TODO 自動生成されたコンストラクター・スタブ
+	
+	/**
+	 * staticに用いられるクラスのためコンストラクタは特に使われない、空です
+	 */
+	public SegmentSetsExtracter() {	
 	}
 	
 	
+//	FIXME ゆくゆくは型を変更する。DataSetとあわせて変更が必要。配列化して高速化したい。
+//	FIXME 最短区間長制限問題をといているが、そもそもの「メチル化率」の定義をしていない
+//	区間に対するメチル化率という言葉の定義が必要。
 	/**
 	 * 
 	 * @param filename
